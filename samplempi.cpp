6e48ca284e494edc66a116ca88bb334e3cad90a2
Kushal Bhandari <kushwiz@gmail.com>
2015-10-29 10:31:22 -0400

    Using C Bindings for now

--- samplempi.cpp
+++ samplempi.cpp
@@ -10,40 +10,48 @@ int main(int argc, char **argv)
 
 	int n, rank, size;
 
-	MPI::Init(argc, argv);
+	MPI_Init(&argc, &argv);
 
-	size = MPI::COMM_WORLD.Get_size();
-	rank = MPI::COMM_WORLD.Get_rank();
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	int p = size;
 	int rootp = sqrt(size);
 	n = 800;
 	int nsquare = pow(n, 2);
 
-	MPI::Status status;
+	MPI_Status status;
 
 	std::cout<<"My Rank:"<<rank<<std::endl;
 
-	MPI::File thefile = MPI::File::Open(MPI::COMM_WORLD, "80x10vec.bin", MPI::MODE_RDONLY, MPI::INFO_NULL);
+	MPI_File thefile;
 
-	MPI::Offset filesize = thefile.Get_size();
+	MPI_File_open(MPI_COMM_WORLD, "80x10vec.bin", MPI_MODE_RDONLY, MPI_INFO_NULL, &thefile);
 
-	MPI::Datatype matrixIntVector = MPI::INT.Create_vector(1, n/rootp, n/rootp);
-	matrixIntVector.Commit();
+	MPI_Offset filesize;
+
+	MPI_File_get_size(thefile, &filesize);
+
+	MPI_Datatype MATRIX_INT_VECTOR;
+
+	MPI_Type_vector(1, n/rootp, n/rootp, MPI_INT, &MATRIX_INT_VECTOR);
+	MPI_Type_commit(&MATRIX_INT_VECTOR);
 
 	int buffr[n/rootp];
 
 	int first_offset = (nsquare / rootp) * floor((rank) / rootp);
 	int second_offset = (rank%rootp) * n / rootp;
 
-		thefile.Read_at((first_offset + second_offset)*sizeof(int), buffr, 1, matrixIntVector, status);
+		MPI_File_read_at(thefile, (first_offset+second_offset)*sizeof(int), buffr, 1, MATRIX_INT_VECTOR, &status);
+		std::cout<<"--"<<rank<<"--";
 		for(int i=0;i<n/rootp;i++)
 		{
 			std::cout<<buffr[i]<<" ";
 		}
 		std::cout<<std::endl;
-	thefile.Close();
+		MPI_File_close(&thefile);
 
-	MPI::Finalize();
+	MPI_Type_free(&MATRIX_INT_VECTOR);
+	MPI_Finalize();
 	return 0;
 }
